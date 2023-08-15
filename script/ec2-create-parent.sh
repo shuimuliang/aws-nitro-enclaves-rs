2784b043a1137fca2d96ddf3a0b3ba32d3001804
shuimuliang <shuimuliang@gmail.com>
2023-08-15 16:25:36 +0800

    build: optimize release build

--- script/ec2-create-parent.sh
+++ script/ec2-create-parent.sh
@@ -6,7 +6,7 @@ aws ec2 run-instances \
 --security-group-ids sg-094c40f8d41b6d337 \
 --enclave-options 'Enabled=true'
 
-# m5.xlarge x86 0.264USD
-# r6g.xlarge ARM 0.268USD
+# m5.xlarge x86 4 vCPU 16 GiB 0.264USD
+# r6g.xlarge ARM 4 vCPU 32 GiB 0.268USD
 # ami-0178f90ef28e37dfc x86
 # ami-08d0b940d663d5ea0 ARM
