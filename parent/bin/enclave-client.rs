dec4479c0872a6e9fc071e7143890f2964464aa8
shuimuliang <shuimuliang@gmail.com>
2023-08-17 22:22:39 +0800

    feat: send iam credential

--- parent/bin/enclave-client.rs
+++ parent/bin/enclave-client.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use serde_json::{Map, Value};
 use vsock::{VsockAddr, VsockStream};
-use parent::{build_payload, recv_message, send_message};
+use parent::{build_payload, recv_message, send_message, get_iam_token};
 
 #[derive(Debug, Parser)]
 struct Opt {
@@ -14,16 +14,19 @@ struct Opt {
     port: u32,
 }
 
-fn main() -> Result<(), anyhow::Error> {
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
     let Opt { cid, port } = Opt::parse();
 
     // Initiate a connection on an AF_VSOCK socket
     let mut stream = VsockStream::connect(&VsockAddr::new(cid, port)).expect("connection failed");
 
     // build payload
-    let credential = Map::new();
-    let key_id = "key_id".to_string();
-    let payload = build_payload("generateAccount", credential, key_id);
+    let credential = get_iam_token().await.unwrap();
+
+    let credential: Map<String, Value> = credential.into_iter().map(|(k, v)| (k, Value::String(v))).collect();
+    let user_id = "UID10000".to_string();
+    let payload = build_payload("generateAccount", credential, user_id);
 
     // send payload
     send_message(&mut stream, payload)?;
